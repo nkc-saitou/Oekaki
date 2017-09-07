961df300764390e027e0290897f39ac706fbdd80
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-09-07 17:25:58 +0900

    車ギミック(震え、動き出すなど)追加

--- Assets/Scripts/SpriteMove.cs
+++ Assets/Scripts/SpriteMove.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class SpriteMove : MonoBehaviour {
 
     //------------------------------------------
+    // public
+    //------------------------------------------
+
+    public float shakeX;
+    public float shakeY;
+
+    //------------------------------------------
     // private
     //------------------------------------------
 
@@ -14,6 +21,8 @@ public class SpriteMove : MonoBehaviour {
     int whitePixels = 0; //最初の白
 
     int pixelsPaint; //どれだけ塗れたかのパーセンテージ
+
+    Vector3 objPos;
 
     void Start ()
     {
@@ -26,8 +35,14 @@ public class SpriteMove : MonoBehaviour {
         {
             PixPaint();
         }
-	}
 
+        ShakeSprite();
+        MoveSprite();
+    }
+
+    //--------------------------------------------------------
+    //　どれだけ色が塗れたかのパーセントを調べるメソッド
+    //--------------------------------------------------------
     void PixPaint()
     {
         filstWhitePixsels = pixCheck.FilstWhitePixels;
@@ -40,7 +55,39 @@ public class SpriteMove : MonoBehaviour {
         p = (float)whitePixels / (float)filstWhitePixsels * 100;
 
         pixelsPaint = 100 - Mathf.FloorToInt(p);
+    }
 
-        Debug.Log(pixelsPaint);
+    //--------------------------------------------------------
+    //　震えるメソッド
+    //--------------------------------------------------------
+    void ShakeSprite()
+    {
+        if (pixelsPaint < 10 || pixelsPaint >= 70) return;
+
+        objPos = gameObject.transform.localPosition;
+
+        objPos.x -= shakeX;
+        objPos.y -= shakeY;
+        shakeX *= -1;
+        shakeY *= -1;
+        objPos.x += shakeX;
+        objPos.y += shakeY;
+
+        gameObject.transform.localPosition = objPos;
+    }
+
+    void MoveSprite()
+    {
+        if (pixelsPaint < 70) return;
+
+        float dx = 0;
+        float speed = 0.5f;
+
+        objPos = gameObject.transform.localPosition;
+        dx += speed * Time.deltaTime;
+        objPos.x += dx;
+        gameObject.transform.localPosition = objPos;
+
+
     }
 }
