66b0c928c418414f13d9c1bf9ffda613bbec0193
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-09-07 14:21:36 +0900

    全体の何パーセントを塗れたかを確認できるように実装

--- Assets/Scripts/PixCheck.cs
+++ Assets/Scripts/PixCheck.cs
@@ -5,30 +5,67 @@ using UnityEngine.UI;
 
 public class PixCheck : MonoBehaviour {
 
+    //------------------------------------------------
+    // private
+    //------------------------------------------------
+
     Texture2D mainTexture;
     Color[] pixels;
 
+    int whitePixels = 0; //現在の白ピクセル数を保存
+    int filstWhitePixels = 0; //一番最初の白ピクセルを保存
+
+    //------------------------------------------------
+    // プロパティ
+    //------------------------------------------------
+
+    //一番最初の白ピクセルを渡すプロパティ
+    public int FilstWhitePixels
+    {
+        get { return filstWhitePixels; }
+        set { filstWhitePixels = value; }
+    }
+
+    //現在白ピクセルを渡すプロパティ
+    public int WhitePixels
+    {
+        get { return whitePixels; }
+        set { whitePixels = value; }
+    }
+
+    //==================================================
+
     void Start ()
     {
+        whitePixelsCheck();
+        filstWhitePixels = whitePixels;
 
     }
 
     void Update ()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetButtonUp("Fire1"))
         {
-            mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
-            pixels = mainTexture.GetPixels();
-
-            int count = 0;
-
-            foreach(Color c in pixels)
-            {
-                if (c == Color.white)
-                    count++;
-            }
-
-            Debug.Log(count);
+            whitePixelsCheck();
         }
-	}
+    }
+
+    //------------------------------------------------
+    // 白のピクセルを調べるメソッド
+    //------------------------------------------------
+    void whitePixelsCheck()
+    {
+        mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
+        pixels = mainTexture.GetPixels();
+
+        int count = 0;
+
+        foreach (Color c in pixels)
+        {
+            if (c == Color.white)
+                count++;
+        }
+
+        whitePixels = count;
+    }
 }
---
+++ Assets/Scripts/SpriteMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMove : MonoBehaviour {
+
+    //------------------------------------------
+    // private
+    //------------------------------------------
+
+    PixCheck pixCheck;
+
+    int filstWhitePixsels;
+    int whitePixels = 0; //最初の白
+
+    int pixelsPaint; //どれだけ塗れたかのパーセンテージ
+
+    void Start ()
+    {
+        pixCheck = GetComponent<PixCheck>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            PixPaint();
+        }
+	}
+
+    void PixPaint()
+    {
+        filstWhitePixsels = pixCheck.FilstWhitePixels;
+        whitePixels = pixCheck.WhitePixels;
+
+        //パーセンテージを計算
+        float p = 0;
+
+        //整数同士の割り算は整数になるので、float型で計算
+        p = (float)whitePixels / (float)filstWhitePixsels * 100;
+
+        pixelsPaint = 100 - Mathf.FloorToInt(p);
+
+        Debug.Log(pixelsPaint);
+    }
+}
