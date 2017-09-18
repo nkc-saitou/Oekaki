381c7678f03e9ca0cc2d8422180e238fd88b58ed
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-09-19 02:26:59 +0900

    花スクリプト修正、ステージ２追加

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -7,39 +7,40 @@ public class Flower : MonoBehaviour {
     public GameObject block;
     Vector3 offset;
     Vector3 target;
-    float deg;
+    float speed = 10.0f;
+    float rad;
 
-	void Start ()
+    bool filstFlg = true;
+
+    void Start()
     {
-        SetTarget(block.transform.localPosition, 60);
+
     }
 
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, 10));
-
+        flowerMove();
     }
 
-    IEnumerator ThrowBall()
+    void flowerMove()
     {
-        float b = Mathf.Tan(deg * Mathf.Deg2Rad);
-        float a = (target.y - b * target.x) / (target.x * target.x);
+        //ラジアン計算
+        // atan2(目標方向のy座標 - 初期位置のy座標, 目標方向のx座標 - 初期位置のx座標)
+        rad = Mathf.Atan2(
+            block.transform.localPosition.y - gameObject.transform.localPosition.y,
+            block.transform.localPosition.x - gameObject.transform.localPosition.x);
 
-        for(float x = 0; x <= this.target.x; x+=0.3f)
-        {
-            float y = a * x * x + b * x;
-            transform.localPosition = new Vector3(x, y, 0) + offset;
-            yield return null;
-        }
-    }
+        //現在のオブジェクトの位置を代入
+        offset = gameObject.transform.localPosition;
 
-    public void SetTarget(Vector3 target, float deg)
-    {
-        this.offset = transform.localPosition;
-        this.target = target - this.offset;
-        this.deg = deg;
+        // x += SPEED * cos(ラジアン)
+        // y += SPEED * sin(ラジアン)
+        offset.x += speed * Time.deltaTime * Mathf.Cos(rad);
+        offset.y += speed * Time.deltaTime * Mathf.Sin(rad);
 
-        StartCoroutine("ThrowBall");
+        //現在の位置へ計算した値を代入する
+        gameObject.transform.localPosition = offset;
     }
 
     void OnTriggerEnter(Collider other)
@@ -49,4 +50,5 @@ public class Flower : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+
 }
