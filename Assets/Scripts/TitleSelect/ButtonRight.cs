268dca02ac3f545fd2867bfb9027f65522229799
HirokiSito <nkc.hiroki.saito@gmail.com>
2017-10-04 18:10:46 +0900

    ImageChange変更

---
+++ Assets/Scripts/TitleSelect/ButtonRight.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonRight : MonoBehaviour
+{
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene("GameScene_01");
+    }
+}
